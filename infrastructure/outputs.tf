0158cd5c0ad2fa032c4bab83cfe74280e0aa4b16
to.ino <theo.hinaut@student.junia.com>
2024-12-16 19:44:05 +0100

    CHORE ( global ) - test deploy

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -6,6 +6,3 @@ output "sql_connection_string"{
     value = module.database.sql_connection_string
 }
 
-output "publish_profile"{
-    value = "https://${module.backend_app.app_name}.azurewebsites.net"
-}
