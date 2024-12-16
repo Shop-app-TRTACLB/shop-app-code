2580162e18e8f020c316607236db6802ebdfc000
to.ino <theo.hinaut@student.junia.com>
2024-12-16 22:11:06 +0100

    CHORE ( global ) - test deploy

--- api/src/main.py
+++ api/src/main.py
@@ -11,8 +11,8 @@ load_dotenv()
 # Créer l'application Flask
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQL_CONNECTION_STRING")  # Chargement depuis .env
+sql_connection_string = os.getenv("SQL_CONNECTION_STRING")
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:"+{sql_connection_string}+",1433;Database=userdb;Uid=adminuser;Pwd={P@ssword123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
