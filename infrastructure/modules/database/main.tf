6f9a981920e091f5a677fa951d89c4aa8771f4f6
to.ino <theo.hinaut@student.junia.com>
2024-12-17 17:43:04 +0100

    CHORE ( global ) test deploy

--- infrastructure/modules/database/main.tf
+++ infrastructure/modules/database/main.tf
@@ -7,14 +7,15 @@
     administrator_login_password = var.admin_password                   # Mot de passe de l'administrateur pour se connecter au serveur SQL
   }
 
- resource "azurerm_mssql_firewall_rule" "app_service_rule" {
-  for_each = toset(var.app_service_ips)
+  resource "azurerm_mssql_firewall_rule" "app_service_rule" {
+    count = length(var.app_service_ips)
 
-  name             = "allow-app-service-${each.key}"  # Nom unique pour chaque IP
-  server_id        = azurerm_mssql_server.server.id
-  start_ip_address = each.value
-  end_ip_address   = each.value
-}
+    name             = "allow-app-service-${count.index}"  # Nom unique basé sur l'index
+    server_id        = azurerm_mssql_server.server.id
+    start_ip_address = var.app_service_ips[count.index]
+    end_ip_address   = var.app_service_ips[count.index]
+  }
+
 
 
   resource "azurerm_mssql_database" "db" {
